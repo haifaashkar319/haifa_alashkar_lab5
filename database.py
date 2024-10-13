c6edafd32de58f09c12d0fa876e0939e04f0cbdd
haifa <hsa53@mail.aub.edu>
2024-10-14 00:21:20 +0300

    Implement REST API endpoints

--- database.py
+++ database.py
@@ -1,3 +1,37 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+
+# Define API endpoints
+
+@app.route('/api/users', methods=['GET'])
+def api_get_users():
+    return jsonify(get_users())
+
+@app.route('/api/users/<user_id>', methods=['GET'])
+def api_get_user(user_id):
+    return jsonify(get_user_by_id(user_id))
+
+@app.route('/api/users/add', methods=['POST'])
+def api_add_user():
+    user = request.get_json()
+    return jsonify(insert_user(user))
+
+@app.route('/api/users/update', methods=['PUT'])
+def api_update_user():
+    user = request.get_json()
+    return jsonify(update_user(user))
+
+@app.route('/api/users/delete/<user_id>', methods=['DELETE'])
+def api_delete_user(user_id):
+    return jsonify(delete_user(user_id))
+
+if __name__ == "__main__":
+    app.run()
+
+
 #!/usr/bin/python
 import sqlite3
 
@@ -126,14 +160,4 @@ def delete_user(user_id):
     return message
 
 
-if __name__ == "__main__":
-    create_db_table()
-    user = {
-        "name": "John Doe",
-        "email": "johndoe@gmail.com",
-        "phone": "123456789",
-        "address": "123 John Doe Street",
-        "country": "Austria"
-    }
-    insert_user(user)
-    print(get_users())
+
